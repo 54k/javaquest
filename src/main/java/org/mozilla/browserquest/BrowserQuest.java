d17f7e213704efd89a62700eb2c4054416c551f5
Korotaev Boris <korotaev@multivi.ru>
2014-09-02 19:43:20 +0600

    +

--- src/main/java/org/mozilla/browserquest/BrowserQuest.java
+++ src/main/java/org/mozilla/browserquest/BrowserQuest.java
@@ -1,26 +1,58 @@
 package org.mozilla.browserquest;
 
-import org.vertx.java.core.http.HttpServer;
+import org.vertx.java.core.json.JsonArray;
+import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.platform.Verticle;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 public class BrowserQuest extends Verticle {
 
-    HttpServer httpServer;
+    private WebSocketServer webSocketServer;
+    private WorldServer[] worlds;
 
     @Override
     public void start() {
-        int port = Optional.ofNullable(getContainer().config().getInteger("port")).orElse(3000);
+        getContainer().logger().info("Starting BrowserQuest server");
 
-        httpServer = getVertx().createHttpServer();
-        httpServer.websocketHandler(ws -> ws.frameHandler(frame -> ws.writeTextFrame(frame.textData())));
-        httpServer.listen(port);
+        JsonObject config = getContainer().config();
+
+        int port = Optional.ofNullable(config.getInteger("serverPort")).orElse(3000);
+        webSocketServer = new WebSocketServer(getVertx());
+        webSocketServer.onStatusRequest(res -> res.end(getWorldDistribution()));
+        webSocketServer.onConnect(ws -> {
+            WorldServer worldServer = getAvailableWorldServer();
+            worldServer.addPlayer(new Player());
+        });
+
+        int worldCount = Optional.ofNullable(config.getInteger("worldCount")).orElse(5);
+        int maxPlayers = Optional.ofNullable(config.getInteger("maxPlayersPerWorld")).orElse(10);
+        populateWorlds(worldCount, maxPlayers);
+
+        webSocketServer.listen(port);
         getContainer().logger().info("BrowserQuest started at port " + port);
     }
 
     @Override
     public void stop() {
-        httpServer.close();
+        webSocketServer.close();
+    }
+
+    private void populateWorlds(int worldCount, int maxPlayers) {
+        worlds = new WorldServer[worldCount];
+        for (int i = 0; i < worldCount; i++) {
+            worlds[i] = new WorldServer("world-" + (i + 1), maxPlayers, webSocketServer);
+        }
+    }
+
+    private WorldServer getAvailableWorldServer() {
+        return Arrays.stream(worlds).filter(w -> w.getPlayersCount() < w.getMaxPlayers()).findFirst().get();
+    }
+
+    private String getWorldDistribution() {
+        JsonArray status = new JsonArray();
+        Arrays.stream(worlds).forEach(w -> status.add(w.getPlayersCount()));
+        return status.encode();
     }
 }
---
+++ src/main/java/org/mozilla/browserquest/WorldServer.java
@@ -0,0 +1,38 @@
+package org.mozilla.browserquest;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class WorldServer {
+
+    private String name;
+    private int maxPlayers;
+    private int playersCount;
+
+    private WebSocketServer server;
+
+    private Set<Player> players = new HashSet<>();
+
+    public WorldServer(String name, int maxPlayers, WebSocketServer server) {
+        this.name = name;
+        this.maxPlayers = maxPlayers;
+        this.server = server;
+    }
+
+    public void addPlayer(Player player) {
+        players.add(player);
+        playersCount++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMaxPlayers() {
+        return maxPlayers;
+    }
+
+    public int getPlayersCount() {
+        return playersCount;
+    }
+}
