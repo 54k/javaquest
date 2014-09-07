900b7715d898b80f169b68fb1228a30c41ae227c
54k <54k@bk.ru>
2014-09-07 23:52:32 +0600

    world map

--- src/main/java/org/mozilla/browserquest/Map.java
+++ src/main/java/org/mozilla/browserquest/Map.java
@@ -1,4 +1,43 @@
 package org.mozilla.browserquest;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.vertx.java.core.file.FileSystem;
+
 public class Map {
+
+    private int width;
+    private int height;
+
+    private double groupWidth;
+    private double groupHeight;
+
+    private int zoneWidth = 28;
+    private int zoneHeight = 12;
+
+    public Map(FileSystem fs, String filePath) {
+        fs.exists(filePath, booleanAsyncResult -> {
+            if (booleanAsyncResult.result()) {
+                fs.readFile(filePath, bufferAsyncResult -> {
+                    byte[] bytes = bufferAsyncResult.result().getBytes();
+                    initMap(bytes);
+                });
+            }
+        });
+    }
+
+    private void initMap(byte[] bytes) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode root = mapper.readTree(bytes);
+
+            width = root.findValue("width").asInt();
+            height = root.findValue("height").asInt();
+
+            groupWidth = Math.floor(width / zoneWidth);
+            groupHeight = Math.floor(height / zoneHeight);
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
 }
