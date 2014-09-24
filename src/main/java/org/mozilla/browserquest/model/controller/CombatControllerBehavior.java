4d2bd49d2ee768a10b4cd872a91cd5c8306f1b10
Korotaev Boris <korotaev@multivi.ru>
2014-09-24 12:25:42 +0600

    +

--- src/main/java/org/mozilla/browserquest/model/controller/CombatControllerBehavior.java
+++ src/main/java/org/mozilla/browserquest/model/controller/CombatControllerBehavior.java
@@ -2,14 +2,20 @@ package org.mozilla.browserquest.model.controller;
 
 import org.mozilla.browserquest.actor.Behavior;
 import org.mozilla.browserquest.actor.BehaviorPrototype;
+import org.mozilla.browserquest.model.Position;
 import org.mozilla.browserquest.model.actor.BQCharacter;
 import org.mozilla.browserquest.model.event.CombatListener;
+import org.mozilla.browserquest.util.PositionUtil;
 
 @BehaviorPrototype(CombatController.class)
 public class CombatControllerBehavior extends Behavior<BQCharacter> implements CombatController {
 
     @Override
     public void attackTarget(BQCharacter target) {
-        target.post(CombatListener.class).onAttack(getActor(), 5);
+        BQCharacter actor = getActor();
+        Position position = PositionUtil.getRandomPositionNear(target);
+        actor.getMovementController().moveTo(position);
+        // TODO calculate damage
+        target.post(CombatListener.class).onAttack(actor, 5);
     }
 }
