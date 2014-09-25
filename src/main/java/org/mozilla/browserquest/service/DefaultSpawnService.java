93e72eb3c1f118ded242ac1a6d9a24a794491178
Korotaev Boris <korotaev@multivi.ru>
2014-09-25 14:07:03 +0600

    +

--- src/main/java/org/mozilla/browserquest/service/DefaultSpawnService.java
+++ src/main/java/org/mozilla/browserquest/service/DefaultSpawnService.java
@@ -5,6 +5,7 @@ import org.mozilla.browserquest.inject.LazyInject;
 import org.mozilla.browserquest.model.Area;
 import org.mozilla.browserquest.model.BQSpawn;
 import org.mozilla.browserquest.model.BQType;
+import org.mozilla.browserquest.template.CheckpointTemplate;
 import org.mozilla.browserquest.template.RoamingAreaTemplate;
 
 import java.util.List;
@@ -16,14 +17,27 @@ public class DefaultSpawnService implements SpawnService {
     @LazyInject
     private DataService dataService;
 
+    private Map<Integer, Area> startingAreas = new ConcurrentHashMap<>();
+    private Map<Integer, Area> spawnAreas = new ConcurrentHashMap<>();
+
     private Map<Integer, BQSpawn> spawns = new ConcurrentHashMap<>();
 
-    @Inject
-    public DefaultSpawnService(List<RoamingAreaTemplate> roamingAreaTemplates) {
-        load(roamingAreaTemplates);
+    public DefaultSpawnService() {
+        loadCheckpoints(dataService.getWorldTemplate().getCheckpoints());
+        loadCreatureSpawns(dataService.getWorldTemplate().getRoamingAreas());
     }
 
-    private void load(List<RoamingAreaTemplate> roamingAreaTemplates) {
+    private void loadCheckpoints(List<CheckpointTemplate> checkpoints) {
+        for (CheckpointTemplate checkpoint : checkpoints) {
+            if (checkpoint.getS() == 1) {
+                startingAreas.put(checkpoint.getId(), new Area(checkpoint.getX(), checkpoint.getY(), checkpoint.getW(), checkpoint.getH()));
+            } else {
+                spawnAreas.put(checkpoint.getId(), new Area(checkpoint.getX(), checkpoint.getY(), checkpoint.getW(), checkpoint.getH()));
+            }
+        }
+    }
+
+    private void loadCreatureSpawns(List<RoamingAreaTemplate> roamingAreaTemplates) {
         for (RoamingAreaTemplate template : roamingAreaTemplates) {
             BQSpawn spawn = createSpawn(template);
             spawns.put(template.getId(), spawn);
