d66c220874bc56cbbe4cb344f2660ad35ee2976b
Korotaev Boris <korotaev@multivi.ru>
2014-10-08 17:46:31 +0600

    +

---
+++ src/main/java/org/mozilla/browserquest/space/MapInstance.java
@@ -0,0 +1,8 @@
+package org.mozilla.browserquest.space;
+
+import org.mozilla.browserquest.actor.Actor;
+import org.mozilla.browserquest.actor.ActorPrototype;
+
+@ActorPrototype(MapInstanceSpaceComponent.class)
+public class MapInstance extends Actor {
+}
